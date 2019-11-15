2312b20b90b575a7f150287682bcb9b4f8cddd6f
Mike Torres <apolloproductionsdj@gmail.com>
2019-11-15 21:56:13 +0000

    added sign in, sign up, sign out links and added user authentification to places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @places = Place.order(:name).paginate(page: params[:page], per_page: 5)
   end 
