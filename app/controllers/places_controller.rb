ea2ac8fcf8956fdd0d72a320f8671473fc8919d4
Mike Torres <apolloproductionsdj@gmail.com>
2019-11-15 17:35:17 +0000

    added pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,12 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.order(:name).paginate(page: params[:page], per_page: 5)
   end 
+
+private
+  def place_params
+    params.require(:place).permit(:name, :address, :description)
+  end
 
 end
